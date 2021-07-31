38aca63ba437a13429e81e071cbc665552250ebe
ferdianatmadiputra <ferdian.atmadiputra@gmail.com>
2021-07-31 16:04:39 +0700

    add route guard

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -2,21 +2,31 @@ import React, { Switch, Route } from 'react-router-dom';
 import LoginPage from './components/pages/LoginPage';
 import MainLayout from './components/pages/MainLayout';
 import WarehouseInboundPage from './components/pages/WarehouseInboundPage';
+import GuardedRoute from './commons/guardedRoute';
+import checkAuth from './commons/checkAuth';
 
 export default function App() {
   return (
-    <MainLayout>
+    <>
       <Switch>
         <Route path="/login" component={LoginPage} />
-        <Route path="/products" component={MainLayout} />
-        <Route path="/warehouse-inbound" component={WarehouseInboundPage} />
-        <Route path="/warehouse-transfer" component={MainLayout} />
-        <Route path="/warehouse-outbound" component={MainLayout} />
-        <Route path="/warehouse-preparing" component={MainLayout} />
-        <Route path="/admin-menu" component={MainLayout} />
-        <Route path="/account" component={MainLayout} />
-        <Route path="/" component={MainLayout} />
       </Switch>
-    </MainLayout>
+      <MainLayout>
+        <Switch>
+          <GuardedRoute
+            path="/warehouse-inbound"
+            component={WarehouseInboundPage}
+            isAuthenticated={checkAuth()}
+          />
+          {/* <Route path="/products" component={MainLayout} /> */}
+          {/* <Route path="/warehouse-transfer" component={MainLayout} /> */}
+          {/* <Route path="/warehouse-outbound" component={MainLayout} /> */}
+          {/* <Route path="/warehouse-preparing" component={MainLayout} /> */}
+          {/* <Route path="/admin-menu" component={MainLayout} /> */}
+          {/* <Route path="/account" component={MainLayout} /> */}
+          {/* <Route path="/" component={MainLayout} /> */}
+        </Switch>
+      </MainLayout>
+    </>
   );
 }
