1a56d279629e85a641fb79885dfc00333e41b2fa
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-05 15:42:06 +0700

    add system to block user if deactivated

--- apps/api/src/auth/auth.guard.ts
+++ apps/api/src/auth/auth.guard.ts
@@ -5,9 +5,14 @@ import {
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import AdminService from '../admin/admin.service';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private adminService: AdminService) {
+    super();
+  }
+
   // eslint-disable-next-line class-methods-use-this
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
@@ -15,7 +20,17 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   }
 
   async canActivate(context: ExecutionContext): Promise<any> {
-    return super.canActivate(context);
+    const result = await super.canActivate(context);
+    if (!result) {
+      return result;
+    }
+    const ctx = GqlExecutionContext.create(context);
+    const { username } = ctx.getContext().req.user;
+    const user = await this.adminService.findOne(username);
+    if (!user.active) {
+      return false;
+    }
+    return result;
   }
 }
 
