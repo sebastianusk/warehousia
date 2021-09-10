237133d8b3c50360c4590c479d826175e1eff64f
ferdianatmadiputra <ferdian.atmadiputra@gmail.com>
2021-09-11 00:22:37 +0700

    skeleten warehouse editor

--- apps/admin/src/app/pages/SuperAdminMenuPage/hooks.tsx
+++ apps/admin/src/app/pages/SuperAdminMenuPage/hooks.tsx
@@ -1,40 +1,40 @@
-import React, { useState, Dispatch, SetStateAction } from 'react';
-import { useQuery, ApolloError } from '@apollo/client';
-import { GET_WAREHOUSES } from '../../graph';
+// import React, { useState, Dispatch, SetStateAction } from 'react';
+// import { useQuery, ApolloError } from '@apollo/client';
+// import { GET_WAREHOUSES } from '../../graph';
 
-interface SuperAdminMenuState {
-  showModalAdd: boolean;
-  setShowModalAdd: Dispatch<SetStateAction<boolean>>;
-  showModalEdit: boolean;
-  setShowModalEdit: Dispatch<SetStateAction<boolean>>;
-  loading: boolean;
-  error: ApolloError | undefined;
-  data: WarehouseType[];
-}
+// interface SuperAdminMenuState {
+//   showModalAdd: boolean;
+//   setShowModalAdd: Dispatch<SetStateAction<boolean>>;
+//   showModalEdit: boolean;
+//   setShowModalEdit: Dispatch<SetStateAction<boolean>>;
+//   loading: boolean;
+//   error: ApolloError | undefined;
+//   data: WarehouseType[];
+// }
 
-type WarehouseType = {
-  name: string;
-  active: boolean;
-  features: string[];
-};
+// type WarehouseType = {
+//   name: string;
+//   active: boolean;
+//   features: string[];
+// };
 
-export default function useSuperAdminMenuHooks(): SuperAdminMenuState {
-  const [showModalAdd, setShowModalAdd] = useState(false);
-  const [showModalEdit, setShowModalEdit] = useState(false);
-  const { loading, error, data } = useQuery(GET_WAREHOUSES, {
-    variables: {
-      query: '',
-      pagination: '',
-    },
-  });
+// export default function useSuperAdminMenuHooks(): SuperAdminMenuState {
+//   const [showModalAdd, setShowModalAdd] = useState(false);
+//   const [showModalEdit, setShowModalEdit] = useState(false);
+//   const { loading, error, data } = useQuery(GET_WAREHOUSES, {
+//     variables: {
+//       query: '',
+//       pagination: '',
+//     },
+//   });
 
-  return {
-    showModalAdd,
-    setShowModalAdd,
-    showModalEdit,
-    setShowModalEdit,
-    loading,
-    error,
-    data,
-  };
-}
+//   return {
+//     showModalAdd,
+//     setShowModalAdd,
+//     showModalEdit,
+//     setShowModalEdit,
+//     loading,
+//     error,
+//     data,
+//   };
+// }
--- apps/admin/src/app/pages/SuperAdminMenuPage/index.tsx
+++ apps/admin/src/app/pages/SuperAdminMenuPage/index.tsx
@@ -1,24 +1,16 @@
-import React, { ReactElement } from 'react';
-import { Button, Input, Card } from 'antd';
-import styles from './index.module.css';
-import ProductListEditor from '../../components/ProductListEditor';
-import ModalAddProduct from '../../components/ModalAddProduct';
-import useSuperAdminMenuHooks from './hooks';
+// import React, { ReactElement } from 'react';
+// import { Button, Input, Card } from 'antd';
+// import styles from './index.module.css';
+// import ProductListEditor from '../../components/ProductListEditor';
+// import ModalAddProduct from '../../components/ModalAddProduct';
+// import useSuperAdminMenuHooks from './hooks';
 
-export default function SuperAdminMenuPage(): ReactElement {
-  const {
-    showModalAdd,
-    setShowModalAdd,
-    showModalEdit,
-    setShowModalEdit,
-    loading,
-    error,
-    data,
-  } = useSuperAdminMenuHooks();
+// export default function SuperAdminMenuPage(): ReactElement {
+//   const { state } = useSuperAdminMenuHooks();
 
-  return (
-    <>
-      <ModalAddProduct visible setVisible />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <ModalAddProduct visible setVisible />
+//     </>
+//   );
+// }
