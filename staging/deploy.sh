4856341b3e1fbf4a54742131e6110f8a49e0c1ca
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-18 09:50:17 +0700

    add deploy system

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -2,6 +2,7 @@
 SERVER=root@128.199.206.195
 DIR=/home/docker
 ssh $SERVER docker-compose -f $DIR/staging/docker-compose.yaml down && true
+ssh $SERVER docker-compose -f $DIR/staging/docker-compose.yaml pull
 # ssh $SERVER rm -rf $DIR/staging
 scp -r ./staging $SERVER:$DIR
 ssh $SERVER docker-compose -f $DIR/staging/docker-compose.yaml up -d
