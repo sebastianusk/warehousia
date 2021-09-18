81eb37f29c98bfc31bc6a37ab3785e2caf38bbe3
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-18 10:47:04 +0700

    add forgotten db-seed

--- staging/db-seed.sh
+++ staging/db-seed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-ssh warehousia-staging -NTL 5433:127.0.0.1:5432 &
+ssh root@128.199.206.195 -NTL 5433:127.0.0.1:5432 &
 pid=$!
 dotenv -e .env.staging -- npx prisma migrate reset
 dotenv -e .env.staging -- npm run db:seed
