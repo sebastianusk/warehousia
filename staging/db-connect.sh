d7201797e4428a5da1b1a2d865c529d44dd53e19
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-18 10:41:50 +0700

    using ip as server

--- staging/db-connect.sh
+++ staging/db-connect.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-ssh warehousia-staging -NTL 5433:127.0.0.1:5432 &
+ssh root@128.199.206.195 -NTL 5433:127.0.0.1:5432 &
 pid=$!
 pgcli -h localhost -p 5433 -U postgres
 echo $pid
--- staging/db-port.sh
+++ staging/db-port.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eux
-ssh warehousia-staging -NTL 5433:127.0.0.1:5432
+ssh root@128.199.206.195 -NTL 5433:127.0.0.1:5432
