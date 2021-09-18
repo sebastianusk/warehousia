f12cb44c03cfa0ce4506366ad54a091bd551b145
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-18 08:52:05 +0700

    change script package json and add seeder shop

--- apps/api/src/seed.ts
+++ apps/api/src/seed.ts
@@ -1,14 +1,15 @@
 import AdminService from './admin/admin.service';
 import AuthWrapper from './auth/auth.wrapper';
 import DBService from './db/db.service';
+import ShopService from './shop/shop.service';
 import { Feature } from './warehouse/warehouse.dto';
 import WarehouseService from './warehouse/warehouse.service';
 
 const db = new DBService();
-
 const adminService = new AdminService(db);
-
 const warehouseService = new WarehouseService(db);
+const shopService = new ShopService(db);
+const auth = new AuthWrapper('bubur');
 
 async function createUser(
   username: string,
@@ -21,26 +22,20 @@ async function createUser(
     .catch(console.log);
 }
 
-async function createLog(username: string, action: string, remarks: any) {
-  await adminService.createLog(username, action, remarks).catch(console.log);
-}
-
 async function createWarehouse(id: string, name: string, features: Feature[]) {
   await warehouseService
-    .createWarehouse(new AuthWrapper('bubur'), id, name, features)
+    .createWarehouse(auth, id, name, features)
     .catch(console.log);
+}
+
+async function createShop(id: string, name: string) {
+  await shopService.createShop(auth, id, name).catch(console.log);
 }
 
 async function seed() {
   await createUser('bubur', 'bubur123', undefined, 'SUPER_ADMIN');
   await createUser('ferdian', 'ferdian123', ['tangerang'], 'ADMIN');
   await createUser('sebo', 'sebo123', ['serpong'], 'ADMIN');
-  await createLog('bubur', 'createOrder', { orderId: 123 });
-  await createLog('bubur', 'changePass', undefined);
-  await createLog('bubur', 'changeRole', undefined);
-  await createLog('bubur', 'createWarehouse', { warehouseId: 555 });
-  await createLog('bubur', 'createOutbound', { outboundId: 1255 });
-  await createLog('bubur', 'createOrder', { orderId: 789 });
   await createWarehouse('tangerang', 'Konter Tangerang', [
     Feature.INBOUND,
     Feature.OUTBOUND,
@@ -50,6 +45,9 @@ async function seed() {
     Feature.INBOUND,
     Feature.OUTBOUND,
   ]);
+  await createShop('tokopedia', 'Tokopedia utama');
+  await createShop('shopee1', 'Shopee Tas');
+  await createShop('shopee2', 'Shopee Gelang');
 }
 
 seed().finally(async () => {
