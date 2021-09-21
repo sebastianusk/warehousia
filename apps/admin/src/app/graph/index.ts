f5e4816db5573faa8da2a8c997b6c69b464fc125
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-21 08:51:12 +0700

    fix error on hit the api with mandatory string

--- apps/admin/src/app/graph/index.ts
+++ apps/admin/src/app/graph/index.ts
@@ -2,7 +2,7 @@
 import { gql } from '@apollo/client';
 
 export const POST_LOGIN = gql`
-  mutation Login($username: String, $password: String) {
+  mutation Login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
       session
     }
