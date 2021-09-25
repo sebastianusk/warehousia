0ab3f904ad6e2d94c7f1f3897a23c09fd08c82e3
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-09-25 13:39:09 +0700

    update schema for products

--- prisma/migrations/20210925063853_init/migration.sql
+++ prisma/migrations/20210925063853_init/migration.sql
@@ -45,10 +45,47 @@ CREATE TABLE "shop" (
 );
 
 -- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "stock" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "warehouse_id" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "stock_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "stocklog" (
+    "id" TEXT NOT NULL,
+    "action" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "stock_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT NOT NULL,
+    "remarks" JSONB,
+
+    CONSTRAINT "stocklog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_admin_access" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "stock_product_id_warehouse_id_key" ON "stock"("product_id", "warehouse_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_admin_access_AB_unique" ON "_admin_access"("A", "B");
@@ -58,6 +95,15 @@ CREATE INDEX "_admin_access_B_index" ON "_admin_access"("B");
 
 -- AddForeignKey
 ALTER TABLE "adminlog" ADD CONSTRAINT "adminlog_username_fkey" FOREIGN KEY ("username") REFERENCES "admin"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "stock" ADD CONSTRAINT "stock_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "stock" ADD CONSTRAINT "stock_warehouse_id_fkey" FOREIGN KEY ("warehouse_id") REFERENCES "warehouse"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "stocklog" ADD CONSTRAINT "stocklog_stock_id_fkey" FOREIGN KEY ("stock_id") REFERENCES "stock"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_admin_access" ADD FOREIGN KEY ("A") REFERENCES "admin"("username") ON DELETE CASCADE ON UPDATE CASCADE;
