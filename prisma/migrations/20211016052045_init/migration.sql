e33abcf376af67644542df1e1ce6dea978c89a24
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-10-16 12:21:34 +0700

    update migration

--- prisma/migrations/20211016052045_init/migration.sql
+++ prisma/migrations/20211016052045_init/migration.sql
@@ -48,6 +48,7 @@ CREATE TABLE "shop" (
 CREATE TABLE "product" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
