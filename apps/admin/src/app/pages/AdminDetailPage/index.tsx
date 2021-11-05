31330be617e3d5f3db1853c64a109250b93b177a
sebastianuskh <sebastianus.kurniawan@gmail.com>
2021-11-05 20:11:34 +0700

    rename file to admin detail

--- apps/admin/src/app/pages/AdminDetailPage/index.tsx
+++ apps/admin/src/app/pages/AdminDetailPage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import AdminDetailHeader from './components/UserDetailHeader';
-import AdminDetailLog from './components/UserDetailLog';
+import AdminDetailHeader from './components/AdminDetailHeader';
+import AdminDetailLog from './components/AdminDetailLog';
 
 export default function AdminDetailPage(): React.ReactElement {
   const { id } = useParams<{ id: string }>();
