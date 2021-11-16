729af9f890a1eb7743c7972db565ad5b46083267
ferdianatmadiputra <ferdian.atmadiputra@gmail.com>
2021-11-16 17:26:33 +0700

    edit admin updates admin list

--- apps/admin/src/app/components/ModalEditUser/hooks.tsx
+++ apps/admin/src/app/components/ModalEditUser/hooks.tsx
@@ -65,7 +65,6 @@ export default function useModalEditUserHooks(
       refetchQueries: [
         {
           query: GET_ADMINS,
-          variables: { query: input.username },
         },
       ],
     });
